367b6fe905976e81fbb8c88eadde6acb8186b7a4
KhalidMimMuzahid <khalidmimm@gmail.com>
2024-02-20 23:56:27 +0600

    python list and list methods

---
+++ python_basics/list.py
@@ -0,0 +1,10 @@
+countries=["United States", "Canada", "France", "Germany", "Bangladesh"] # list
+countries2= list(("United States", "Canada", "France", "Germany", "Bangladesh")) #list using list constructor
+ 
+print(countries)
+
+print(countries[1:]) #it will print from index 1 to the last index of this list
+print(countries[1:3]) #it will print from index 1 to the before  of index 3
+
+print(countries[-1]) # last element of this list
+print(countries[-2]) # second last element of this list
