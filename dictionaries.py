a2c43ebc7b0462e94c962356d96bff57c6ba090c
KhalidMimMuzahid <khalidmimm@gmail.com>
2024-02-21 11:16:14 +0600

    dict in python

---
+++ dictionaries.py
@@ -0,0 +1,14 @@
+my_dict={
+    "name":"Khalid", #here "name" is key/field  and "Khalid" is value
+    "nationality": "Bangladeshi",
+    "age": 26,
+    "is_tall": True,
+    "Qualification": "Bsc in CSE",
+    "friends": ["Ashik", "Shameem", "Rakib"],
+} #<class 'dict'>
+x= my_dict["name"]
+print(x)
+print(my_dict)
+print(my_dict["friends"])
+print(len(my_dict))
+print(type(my_dict))
